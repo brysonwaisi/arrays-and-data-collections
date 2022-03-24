b698790ae099f75e6a0b594a4a2183dace44da20
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-24 11:39:46 +0300

    Maps

--- Working with maps/js/page.js
+++ Working with maps/js/page.js
@@ -13,9 +13,39 @@ var yearlyTotal = 0;
 
 const monthlySales = new Map();
 
+{
+	let salesA = {
+		a:[1, 2]
+	}
+
+	var map = new WeakMap();
+	map.set(salesA, 'Hiking');
+}
+
 // Add Sales
 function addSale(){
-	monthlySales.set(newMonth.value, newAmount.value)
+	monthlySales.set(newMonth.value, parseInt(newAmount.value))
+
+	//updating labels
+
+	monthlySalesChart.data.labels = Array.from(monthlySales.keys())
+	
+	yearlyTotal = 0;
+	monthlySalesChart.data.datasets.forEach(dataset => {
+		dataset.data = [];
+	});
+
+	for (let amount of monthlySales.values()) {
+		yearlyTotal = amount + yearlyTotal;
+		yearlyLabel.innerHTML = yearlyTotal;
+
+		monthlySalesChart.data.datasets.forEach(dataset => {
+			dataset.data.push(amount);
+		})
+	}
+	monthlySalesChart.update()
+
+
 }
 
 function findSale(){
@@ -27,31 +57,31 @@ function fillValue() {
 }
 
 // Bar chart
-// var monthlySalesChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: [],
-//         datasets: [{
-//             label: '# of Sales',
-//             data: [],
-//             backgroundColor: [
-//                 'rgba(238, 184, 104, 1)',
-//                 'rgba(75, 166, 223, 1)',
-//                 'rgba(239, 118, 122, 1)',
-//             ],
-//             borderWidth: 0
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+var monthlySalesChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# of Sales',
+            data: [],
+            backgroundColor: [
+                'rgba(238, 184, 104, 1)',
+                'rgba(75, 166, 223, 1)',
+                'rgba(239, 118, 122, 1)',
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
 // Pie Chart
 // var deptSalesChart = new Chart(pieCtx, {
